ada96a3ad644366084312808c952d10749a9ebbf
Ekaterina123 <e.efimova@2gis.ru>
2016-04-13 19:13:01 +0600

    Задание 2. Ввести с консоли 2 числа: n и iю Вывести значение i-ого бита числа n.

---
+++ Lecture1_Task2/Lecture1_Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture1_Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = ((Convert.ToInt32(Console.ReadLine()) >> Convert.ToInt32(Console.ReadLine())) & 1);
+            Console.WriteLine(x);
+        }
+    }
+}
