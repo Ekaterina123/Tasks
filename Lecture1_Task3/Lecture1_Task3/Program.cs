e4bc967adaf852d97f95ad2d72509fe06532ae58
Ekaterina123 <e.efimova@2gis.ru>
2016-04-14 10:14:47 +0600

    Задание 3. Ввести с консоли число. Обнулить последний бит этого числа. Вывести на консоль.

---
+++ Lecture1_Task3/Lecture1_Task3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture1_Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int aa = a >> 1;
+            a = aa << 1;
+            Console.WriteLine(a);
+            Console.ReadKey();
+        }
+    }
+}
