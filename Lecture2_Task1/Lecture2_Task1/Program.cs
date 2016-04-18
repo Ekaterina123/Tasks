fdfdbd4f191fa7a0dc31f5fc337114d3dbbc1fbb
Ekaterina123 <e.efimova@2gis.ru>
2016-04-18 20:21:08 +0600

    2

--- Lecture2_Task1/Lecture2_Task1/Program.cs
+++ Lecture2_Task1/Lecture2_Task1/Program.cs
@@ -12,26 +12,65 @@ namespace Lecture2_Task1
         {
             int n = Convert.ToInt32(Console.ReadLine());
             int i = 0, Sum = 0;
-            int Maximal = 0;
-            int Minimal = 0, CountEven = 0, CountNotEven = 0, Composition = 0;
+            int max = 0;
+            int Minimal = 0, CountEven = 0, CountNotEven = 0, Composition = 1;
             for (; i < n; i++)
             {
                 int a = Convert.ToInt32(Console.ReadLine());
-                Sum = Sum + a;
-                if (Maximal < a)
+                if (i == 1)
                 {
-                    Maximal = a
+                    max = a;
+                    Minimal = a;
+                }
+
+                if (max < a)
+                {
+                    max = a;
                 }
 
                 if (Minimal > a)
                 {
-                    Minimal = a
+                    Minimal = a;
                 }
+                if (a % 2 == 0)
+                {
+                    CountEven++;
+                }
+                if (a % 2 == 1)
+                {
+                    CountNotEven++;
+                    Composition = Composition * a;  // еще ошибка
+                }
+                Sum = Sum + a;
             }
+            Console.Write("Сумма:");
             Console.Write(Sum);
-            Console.Write(Maximal);
+            Console.ReadKey();
+            Console.Write("Максимальное значение:");
+            Console.Write(max);
+            Console.ReadKey();
+            Console.Write("Минимальное значение:");
             Console.Write(Minimal);
             Console.ReadKey();
+            Console.Write("Количество четных чисел:");
+            Console.Write(CountEven);
+            Console.ReadKey();
+            Console.Write("Количество нечетных чисел:");
+            Console.Write(CountNotEven);
+            Console.ReadKey();
+            if (Composition ==1)
+            {
+                Console.Write("Произведение нечетных чисел: не было веедено ни одного нечетного числа");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.Write("Произведение нечетных чисел:");
+                Console.Write(Composition);
+                Console.ReadKey();
+
+            }
+
         }
     }
 }
