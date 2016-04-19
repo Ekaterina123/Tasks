42ab8ecee007ce0fc57ea44a3a8b1eb751f2895c
Ekaterina123 <e.efimova@2gis.ru>
2016-04-19 15:32:35 +0600

    1

---
+++ Lecture2_Task2/Lecture2_Task2/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Lecture2_Task2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("2ГИС")]
+[assembly: AssemblyProduct("Lecture2_Task2")]
+[assembly: AssemblyCopyright("Copyright © 2ГИС 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b252002a-98e0-4a8a-8662-c7002ebf9835")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Lecture3_Task1/Lecture3_Task1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture3_Task1
+{
+    class Program
+    {
+
+        
+        private static int chet(int x)  
+            {
+            if (x % 2 == 0)
+            {
+                return x;
+            }
+            return 0;
+            }
+            
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = chet(a);
+            Console.WriteLine(b);
+        }
+    }
+}
