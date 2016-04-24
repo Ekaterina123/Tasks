7d8f9c1f8dcaa23c45339ea878397bfbb4f13ad7
Ekaterina123 <e.efimova@2gis.ru>
2016-04-24 18:22:54 +0600

    Заполнить массив длиной N случайными числами. Ввести с консоли число а. Вывести Yes, если число А есть в массиве, No - в противном случае.

---
+++ Homework/Lecture2_Homework/Lecture2_Homework/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture2_Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число N:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] num1 = new int[n];
+            Console.WriteLine("Введите число А от 0 до 10:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Random rnd = new Random();
+            int x = 0;
+
+            Console.WriteLine("Massiv:");
+            for (int i = 0; i < n; i++)
+            {
+                num1[i] = rnd.Next(0,10);
+                Console.WriteLine(num1[i]);
+            }
+            for (int i = 0; i < n; i++)
+            {
+                if (num1[i] == a)
+                {
+                    x = 1;
+                    break;
+                }
+            }
+
+            if (x == 0)
+            {
+                Console.WriteLine("No");
+            }
+            else Console.WriteLine("Yes");
+
+        }
+    }
+}
