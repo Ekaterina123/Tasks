a835f9ec48265a746340e72e4ea9cdf9db3b52a8
Ekaterina123 <e.efimova@2gis.ru>
2016-04-24 16:56:43 +0600

    Задание 2: Заполнить с консоли массив из N элементов. Отсортировать. Вывести результат. N - задается с консоли.

--- Lecture2_Task2/Lecture2_Task2/Program.cs
+++ Lecture2_Task2/Lecture2_Task2/Program.cs
@@ -10,6 +10,24 @@ namespace Lecture2_Task2
     {
         static void Main(string[] args)
         {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] numbers1 = new int[n];
+            int i = 0;
+            int k = 0;
+
+            for (; i < n; i++ )
+            {
+                numbers1[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            
+            Array.Sort(numbers1);
+            
+            for (; k < n; k++)
+            {
+                Console.WriteLine(numbers1[k]); 
+            }
+
+
         }
     }
 }
