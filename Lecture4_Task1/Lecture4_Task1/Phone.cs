8541d623bfb2857b06004c2e8dffa08826e47856
Ekaterina123 <e.efimova@2gis.ru>
2016-05-03 13:43:48 +0600

    Задание 1: Создать класс “Телефон”, который содержит следующие данные: код города, номер телефона, которые не могут быть изменены. Добавить метод, который возвращает строку вида “(999) 999999” или “999999”, если код города отсутствует.

---
+++ Lecture4_Task1/Lecture4_Task1/Phone.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture4_Task1
+{
+    public class Phone
+    {
+        public readonly string Code;
+        public readonly string PhoneNumber;
+
+        public Phone(string code, string phoneNumber)
+        {
+            Code = code;
+            PhoneNumber = phoneNumber;
+        }
+        public string PrintPhone()
+        {
+            if (Code == null || Code == "")
+            {
+                return PhoneNumber;
+            }
+            else
+            {
+                return "(" + Code + ")" + PhoneNumber;
+            }
+        }
+    }
+}
---
+++ Lecture4_Task1/Lecture4_Task1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture4_Task1
+{
+    class Program
+    {
+        //Создать класс “Телефон”, который содержит следующие данные:
+        //код города, номер телефона, которые не могут быть изменены.
+        //Добавить метод, который возвращает строку вида “(999) 999999” 
+        //или “999999”, если код города отсутствует.
+
+        static void Main(string[] args)
+        {
+
+            Phone p = new Phone("999", "999999");
+            Console.WriteLine(p.PrintPhone());
+        }
+    }
+}
